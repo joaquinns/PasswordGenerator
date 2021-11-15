6190cf70419afe0627b26d9b7db7ee0371bd3b02
Joaquin <76413456+joaquinns@users.noreply.github.com>
2021-11-15 13:11:47 -0400

    Update settings.py

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,11 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
- 
-MEDIA_URL = '/static/uploads/'
-MEDIA_ROOT = os.path.join(STATIC_ROOT, "uploads")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
